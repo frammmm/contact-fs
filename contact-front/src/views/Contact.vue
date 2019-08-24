6d01cbead9393094f88c3eb4f932f00addaa5fef
Amanaemonesia <frammm1337@gmail.com>
2019-08-25 00:18:10 +0500

    fix: eslint 'max-attributes-per-line' warning

--- contact-front/src/views/Contact.vue
+++ contact-front/src/views/Contact.vue
@@ -4,18 +4,18 @@
     <form @submit.prevent="handleSubmit" class="contact-form">
       <div class="form-group">
         <label for="header">Тема:</label>
-        <input 
-          v-model="header" 
-          class="form-control" 
-          type="text" 
-          id="header" 
-          name="header" 
+        <input
+          v-model="header"
+          class="form-control"
+          type="text"
+          id="header"
+          name="header"
           required
         >
       </div>
       <div class="form-group">
         <label for="message">Сообщение:</label>
-        <textarea 
+        <textarea
           v-model="body"
           class="form-control"
           name="message"
@@ -25,10 +25,15 @@
         ></textarea>
       </div>
       <div class="form-group">
-        <input @change="handleFileAttach" type="file" name="attach" id="attach">
+        <input
+          @change="handleFileAttach"
+          type="file"
+          name="attach"
+          id="attach"
+        >
       </div>
 
-     <div
+      <div
         v-if="error"
         class="alert alert-warning"
       >
@@ -40,36 +45,36 @@
   </div>
 </template>
 <script>
-import { mapState } from "vuex";
+  import { mapState } from 'vuex';
 
-export default {
-  computed: {
-    ...mapState({
-      error: state => state.request.contactError
-    })
-  },
-  data() {
-    return {
-      header: "",
-      body: "",
-      file: null
-    };
-  },
-  methods: {
-    handleFileAttach: function(e) {
-      this.file = e.target.files[0];
+  export default {
+    computed: {
+      ...mapState({
+        error: state => state.request.contactError,
+      }),
     },
+    data() {
+      return {
+        header: '',
+        body: '',
+        file: null,
+      };
+    },
+    methods: {
+      handleFileAttach: function (e) {
+        this.file = e.target.files[0];
+      },
 
-    handleSubmit: function() {
-      const formdata = new FormData();
-      formdata.append("header", this.header);
-      formdata.append("body", this.body);
-      formdata.append("file", this.file);
-      this.$store.dispatch("createRequest", {
-        data: formdata,
-        token: this.$store.state.user.token
-      });
-    }
-  }
-};
+      handleSubmit: function () {
+        const formdata = new FormData();
+        formdata.append('header', this.header);
+        formdata.append('body', this.body);
+        formdata.append('file', this.file);
+        this.$store.dispatch('createRequest', {
+          data: formdata,
+          token: this.$store.state.user.token,
+        });
+      },
+    },
+  };
 </script>
